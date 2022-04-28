0f3ab6eda2eb4a2c7082c31ff70bb82093c214f4
Natan__Brito <natanismaelbrito@gmail.com>
2022-04-28 11:26:12 -0300

    mexendo no post/participants

--- index.js
+++ index.js
@@ -3,17 +3,30 @@ import cors from "cors";
 import chalk from "chalk";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+import dayjs from "dayjs";
 // ...
 //npm run dev (pra derrubar/levantar o server de nov)
 const app=express();
-dotenv.config();
 app.use(cors());
 app.use(express.json());
+dotenv.config();
 app.listen(5000,()=>{console.log(chalk.bold.green("Silencio, estamos no AR!!!"))})
-
-app.post("/participants",(req,res)=>{
-    const {name} =req.body;
- res.send("sem, validações")
+let db=null;
+const mongoClient= new MongoClient("mongodb://localhost:27017");// .env nao funfando
+app.post("/participants", async (req,res)=>{
+    const {name} =req.body;//dayjs().format('HH:MM:ss')
+    const user={name: 'xxx', lastStatus: Date.now()} 
+    try{
+        await mongoClient.connect();
+        const dataBaseUsers=mongoClient.db("users")
+        const UsersData=await dataBaseUsers.collection("users").insertOne(user)  
+        res.sendStatus(200)
+        mongoClient.close();
+    }catch(e){
+        console.log("deu erro patrão")
+        res.sendStatus(500)
+        mongoClient.close();
+    }
 })
 app.post("/messages",(req,res)=>{
     const from = req.headers.user; //ver se tem na lista de participantes
