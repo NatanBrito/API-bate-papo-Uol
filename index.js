291ffa4dad98757efaa86be71c83f1cd548a58bb
Natan__Brito <natanismaelbrito@gmail.com>
2022-04-28 23:08:22 -0300

    get/messages faltando o joi

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import express from "express";
 import cors from "cors";
 import chalk from "chalk";
 import { MongoClient } from "mongodb";
-import dotenv from "dotenv";
 import dayjs from "dayjs";
+import dotenv from "dotenv";
 // ...
 //npm run dev (pra derrubar/levantar o server de nov)
 const app=express();
@@ -14,12 +14,11 @@ app.listen(5000,()=>{console.log(chalk.bold.green("Silencio, estamos no AR!!!"))
 let db=null;
 const mongoClient= new MongoClient("mongodb://localhost:27017");// .env nao funfando
 app.post("/participants", async (req,res)=>{
-    const {name,from,to,text,type} =req.body;
     const user={name: 'xxx', lastStatus: Date.now()} 
     try{
         await mongoClient.connect();
         const dataBaseUsers=mongoClient.db("users")
-        const UsersData=await dataBaseUsers.collection("users").insertOne(user)  
+        await dataBaseUsers.collection("users").insertOne(user)  
         res.sendStatus(200)
 
         mongoClient.close();
@@ -32,8 +31,8 @@ app.post("/participants", async (req,res)=>{
 app.get("/participants", async (req,res)=>{
     try{
         await mongoClient.connect();
-        const db=mongoClient.db("users");
-        const returnUsers= await db.collection("users").find({}).toArray();
+        const dataBase=mongoClient.db("users");
+        const returnUsers= await dataBase.collection("users").find({}).toArray();
         res.send(returnUsers);
         mongoClient.close();
     }catch(e){
@@ -48,15 +47,34 @@ app.post("/messages",async (req,res)=>{
     try{
         await mongoClient.connect();
         const messages=mongoClient.db("messages")
-        const sendMessage= await messages.collection("messages").insertOne(message)
-        res.send("sem, validações")
+        await messages.collection("messages").insertOne(message)
+        res.send(xx)
         mongoClient.close();
     } catch(e){
         res.sendStatus(500)
         mongoClient.close();
     }
 })
-app.get("/messages",async(req,res)=>{
-    const {limit}=req.params // ver se ta certo e meter o parseInt no number
-   
+app.get("/messages",async (req,res)=>{
+    const user=req.headers.user;
+    let limit=(req.query.limit) // ver se ta certo e meter o parseInt no number
+    try{
+        await mongoClient.connect();
+        const db=mongoClient.db("messages")
+        const messages= await db.collection("messages").find({}).toArray();
+        const userMessages=[];
+        for(let i=0;i<messages.length;i++){
+           if(messages[i].to === user || messages[i].type !== "private_message"){
+               userMessages.push(messages[i])
+            }
+        }
+        if(limit === undefined){
+            limit= messages.length;
+        }
+        res.send(userMessages.splice(0,parseInt(limit)))
+        mongoClient.close();
+    }catch(e){
+        res.status(404).send("ta vacilando em moscão")
+        mongoClient.close();
+    }
 })
