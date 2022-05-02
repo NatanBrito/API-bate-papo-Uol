f95ba79556c0a78ad6874346852185d565a57391
Natan__Brito <natanismaelbrito@gmail.com>
2022-05-02 16:41:54 -0300

    save under end

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ import chalk from "chalk";
 import { MongoClient, ObjectId } from "mongodb";
 import dayjs from "dayjs";
 import dotenv from "dotenv";
-import Join from "join";
-// ...
-//npm run dev (pra derrubar/levantar o server de nov)
+import  joi from "joi";
+const postschema=joi.object({
+    
+})
 const app=express();
 app.use(cors());
 app.use(express.json());
@@ -15,18 +16,34 @@ app.listen(5000,()=>{console.log(chalk.bold.green("Silencio, estamos no AR!!!"))
 const mongoClient= new MongoClient("mongodb://localhost:27017");// .env nao funfando
 app.post("/participants", async (req,res)=>{
     const {name}=req.body;
+    
     console.log("cheguei",name)
-    const user={name, lastStatus: Date.now()} 
+    let user={name, lastStatus: Date.now()} 
+    const userSchema= joi.object({
+        name: joi.string().required(),
+        lastStatus:joi.number()
+    })
+    const validate= userSchema.validate(user)
+    if(validate.error){
+        res.status(422).send(validate.error)
+        return;
+    }
     try{
         await mongoClient.connect();
         const dataBaseUsers=mongoClient.db("projeto-Uol")
+        const search= await dataBaseUsers.collection("users").findOne({name}) 
+        if(search){
+            res.status(409).send("repetido")
+            return;
+        }
+          console.log(search)
         await dataBaseUsers.collection("users").insertOne(user)  
-        res.sendStatus(200)
+        res.sendStatus(201)
 
         mongoClient.close();
     }catch(e){
         console.log("deu erro patrão")
-        res.sendStatus(500)
+        res.sendStatus(422)
         mongoClient.close();
     }
 })
@@ -43,14 +60,35 @@ app.get("/participants", async (req,res)=>{
     }
 })
 app.post("/messages",async (req,res)=>{
-    const from = req.headers.user; //ver se tem na lista de participantes
-    const {to,text,type} =req.body; //validar todas as informações
+    const messageSchema= joi.object({
+        from: joi.string().min(1).required(),
+        to: joi.string().min(1).required(),
+        text: joi.string().min(1).required(),
+        type: joi.any().valid("message", "private_message").required(),
+        time: joi.string().required()
+    })
+    const from = req.headers.user; 
+    const {to,text,type} =req.body;
     const message={from,to,text,type,time:dayjs().format('HH:MM:ss')};
+    const validate= messageSchema.validate(message)
+    if(validate.error){
+        res.status(409).send(validate.error.details)
+        return;
+    }
     try{
         await mongoClient.connect();
         const messages=mongoClient.db("projeto-Uol")
+        const onParticipant= await messages.collection("users").find({name:from}).toArray();
+        console.log("aqui",{onParticipant})
+        if(onParticipant.length===0){
+            res.sendStatus(422);
+            console.log("não achei ngm",{onParticipant})
+            mongoClient.close();
+            return;
+        }
         await messages.collection("messages").insertOne(message)
-        res.send(xx)
+        
+        res.sendStatus(201)
         mongoClient.close();
     } catch(e){
         res.sendStatus(500)
@@ -59,6 +97,7 @@ app.post("/messages",async (req,res)=>{
 })
 app.get("/messages",async (req,res)=>{
     const user=req.headers.user;
+    console.log({user})
     let limit=(req.query.limit) // ver se ta certo e meter o parseInt no number
     try{
         await mongoClient.connect();
@@ -66,8 +105,9 @@ app.get("/messages",async (req,res)=>{
         const messages= await db.collection("messages").find({}).toArray();
         const userMessages=[];
         for(let i=0;i<messages.length;i++){
-           if(messages[i].to === user || messages[i].type !== "private_message"){
-               userMessages.push(messages[i])
+           if(messages[i].to === user || messages[i].type !== "private_message"||
+              messages[i].type === "status"||messages[i].from === user){
+                 userMessages.push(messages[i])
             }
         }
         if(!limit){
@@ -96,18 +136,39 @@ try{
     console.log(findUser)
     mongoClient.close();
 }catch(e){
-    res.sendStatus(404)
+    res.sendStatus(409)
     mongoClient.close();
 }
 })
     setInterval(async()=>{
+        console.log("comecei")
         try{ 
         await mongoClient.connect();
         const list=mongoClient.db("projeto-Uol");
-        const removeUser= await list.collection("users").find({}).toArray();
-        console.log(removeUser[0]) 
+        const usuario= await list.collection("users").find({}).toArray();
+        mongoClient.close();
          }catch(e){
              res.send("foi de base burrão")
+             mongoClient.close();
          }
+         usuario.forEach(async(user)=>{
+             let now= Date.now();
+             let time= now-user.lastStatus;
+             if(time>15000){
+               try{
+                    await mongoClient.connect();
+                    const db=mongoClient.db("projeto-Uol")
+                    await db.collection("users").deleteOne({_id:user._id})
+                    await db.collection("messages").insertOne({from:user.name, to: 'Todos', text: 'sai da sala...', type: 'status', time:dayjs().format('HH:MM:ss') })
+                    console.log("terminei com sucesso")
+                    res.sendStatus(200)
+                    mongoClient.close();
+               } catch(e){
+                   res.sendStatus(409)
+                   mongoClient.close();
+                }
+             }
+          })
+
      },15000)
 
